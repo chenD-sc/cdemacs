4532d4af240b3f011df9df32a302172c4822c3ee
psil-or <psil_jy@outlook.com>
2020-03-06 17:55:25 +0800

    Update init; Add submodule: dash, smex, transient, with-editor

--- init.el
+++ init.el
@@ -1,19 +1,14 @@
-;;; oremacs
 ;;* Base directory and load-path
 (defvar emacs-d
   (file-name-directory
    (file-chase-links load-file-name))
   "The giant turtle on which the world rests.")
-
-(defun add-subdirs-to-load-path (dir)
+(defun ora-subdirs-add (dir)
   "Recursive add directories to `load-path'."
   (let ((default-directory (file-name-as-directory dir)))
     (add-to-list 'load-path dir)
     (normal-top-level-add-subdirs-to-load-path)))
-(add-subdirs-to-load-path (expand-file-name "site-lisp/" emacs-d))
-
-(add-to-list 'load-path (expand-file-name "modes/" emacs-d))
-(setq enable-local-variables :all)
+(ora-subdirs-add (expand-file-name "site-lisp/" emacs-d))
 
 ;;* Font
 (defun ora-set-font (&optional frame)
@@ -25,11 +20,11 @@
     (error
      (ignore-errors
        (set-frame-font
-	"Lucida Sans Typewriter")))))
+        "Lucida Sans Typewriter")))))
 (ora-set-font)
-(set-face-attribute 'default nil :height (if (eq system-type 'drawin) 120 113))
+(set-face-attribute 'default nil :height (if (eq system-type 'darwin) 120 113))
 (ignore-errors
-  (set-fontset-font t nil "symbol" nil 'append))
+  (set-fontset-font t nil "Symbola" nil 'append))
 (add-hook 'after-make-frame-functions 'ora-set-font)
 ;;* Customize
 (defmacro csetq (variable value)
@@ -41,10 +36,6 @@
 (csetq tool-bar-mode nil)
 (csetq menu-bar-mode nil)
 (csetq scroll-bar-mode nil)
-(csetq truncate-lines t)
-(csetq inhibit-startup-screen t)
-(csetq initial-scratch-message "")
-(csetq text-quoting-style 'grave)
 (csetq line-number-display-limit-width 2000000)
 ;;** navigation within buffer
 (csetq next-screen-context-lines 5)
@@ -55,31 +46,16 @@
 (csetq read-file-name-completion-ignore-case t)
 (csetq read-buffer-completion-ignore-case t)
 (prefer-coding-system 'utf-8)
-;;* minibuffer interaction
+;;** minibuffer interaction
 (csetq enable-recursive-minibuffers t)
-(setq minibuffer-message-timeout t)
+(setq minibuffer-message-timeout 1)
 (minibuffer-depth-indicate-mode 1)
 (csetq read-quoted-char-radix 16)
 ;;** editor behavior
-(csetq indent-tabs-mode nil)
-(csetq ring-bell-function 'ignore)
-(csetq highlight-nonselected-windows nil)
 (defalias 'yes-or-no-p 'y-or-n-p)
-(setq kill-buffer-query-functions nil)
-(add-hook 'server-switch-hook 'raise-frame)
-(defadvice set-window-dedicated-p (around no-dedicated-windows activate))
-(remove-hook 'post-self-insert-hook 'blink-paren-post-self-insert-function)
 (csetq eval-expression-print-length nil)
 (csetq eval-expression-print-level nil)
-(setq print-gensym nil)
-(setq print-circle nil)
-(setq byte-compile--use-old-handlers nil)
-;; http://debbugs.gnu.org/cgi/bugreport.cgi?bug=16737
-(setq x-selection-timeout 10)
-;; imporve copying from a ssh -X Emacs.
-(setq x-selection-timeout 100)
-(csetq lpr-command "gtklp")
-;;** internals
+;;** Internals
 (csetq gc-cons-threshold (* 10 1024 1024))
 (csetq ad-redefinition-action 'accept)
 ;;** time display
@@ -89,26 +65,18 @@
 ;;** Rest
 (csetq browse-url-browser-function 'browse-url-firefox)
 (csetq browse-url-firefox-program "firefox")
-;;*** Backups
-(setq backup-by-copying t)
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
-(setq delete-old-versions t)
-(setq version-control t)
-(setq create-lockfiles nil)
 ;;* Bootstrap
-;;** autoloads
-
 ;;** enable features
 (mapc (lambda (x) (put x 'disabled nil))
       '(erase-buffer upcase-region downcase-region
         dired-find-alternate-file narrow-to-region))
-;;** package.el
+;;** theme
 (let ((file-name-handler-alist nil))
-  (require 'eclipse-theme)
-  (load-theme 'eclipse t)
-  (require 'smex))
+  (require 'eclipse-theme))
+
+;;* Modes
+;;** global minor modes 
+(with-temp-message ""
+  (require 'lispy))
 
 
-;;*
-(require 'lispy)
-(require 'magit)
