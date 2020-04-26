b676c65d9b4832579cd9357e00e2fd856fc6ad1b
psil-or <psil_jy@outlook.com>
2020-04-26 19:52:37 +0800

    Add config file

---
+++ config/init-backup.el
@@ -0,0 +1,5 @@
+;; Disable auto backup, auto-save.el is fine
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(provide 'init-backup)
---
+++ config/init-buffer-menu.el
@@ -0,0 +1,8 @@
+(require 'hydra)
+(defhydra hydra-buffer-menu (:color pink
+                                    :hint nil)
+  "
+^Mark^        ^Unmark^      ^Action^
+
+"
+  )
